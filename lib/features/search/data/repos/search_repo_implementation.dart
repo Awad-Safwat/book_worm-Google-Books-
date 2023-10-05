c417a31db050297724d9d99201bf816096b649bc
Awad-Safwat <awad.safwat1999@gmail.com>
2023-10-05 18:27:36 +0300

    added search repo imple to data layer to search feature

---
+++ lib/features/search/data/repos/search_repo_implementation.dart
@@ -0,0 +1,30 @@
+import 'package:book_worm/core/errors/faluer.dart';
+import 'package:book_worm/features/search/data/data_sources/search_remote_data_source.dart';
+import 'package:book_worm/features/search/domain/entities/searched_book_entity.dart';
+import 'package:book_worm/features/search/domain/repos/search_view_repo.dart';
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+
+class SearchRepoImplementation extends SearchViewRepo {
+  final SearchRemoteDataSource searchRemoteDataSource;
+
+  SearchRepoImplementation({required this.searchRemoteDataSource});
+  @override
+  Future<Either<Faluer, List<SearchedBookEntity>>> fetchHistorySearchedBooks() {
+    throw 'e';
+  }
+
+  @override
+  Future<Either<Faluer, List<SearchedBookEntity>>> fetchSearchedBooks() async {
+    List<SearchedBookEntity> books;
+    try {
+      books = await searchRemoteDataSource.fetchSearchedBooks();
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFalure.fromDioError(e));
+      }
+      return left(ServerFalure(massege: e.toString()));
+    }
+  }
+}
